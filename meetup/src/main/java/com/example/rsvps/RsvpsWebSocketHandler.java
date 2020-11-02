202ec18271aba6bda5c5eb5a4b6592581ed46cf3
Aishwarya shirse <adityashirse84@gmail.com>
2020-11-02 15:14:34 +0530

    spring cloud stream(Sending data to kafka)

--- meetup/src/main/java/com/example/rsvps/RsvpsWebSocketHandler.java
+++ meetup/src/main/java/com/example/rsvps/RsvpsWebSocketHandler.java
@@ -5,12 +5,26 @@ import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Component
 class RsvpsWebSocketHandler extends AbstractWebSocketHandler {
+    private static final Logger logger =
+            Logger.getLogger(RsvpsWebSocketHandler.class.getName());
+
+    private final RsvpsKafkaProducer rsvpsKafkaProducer;
+
+    public RsvpsWebSocketHandler(RsvpsKafkaProducer rsvpsKafkaProducer) {
+        this.rsvpsKafkaProducer = rsvpsKafkaProducer;
+    }
 
     @Override
     public void handleMessage(WebSocketSession session,
                               WebSocketMessage<?> message) {
-        System.out.println(message.getPayload());
+        //System.out.println(message.getPayload());
+        logger.log( Level.INFO, "New RSVP:\n {0}", message.getPayload());
+
+        rsvpsKafkaProducer.sendRsvpMessage(message);
     }
 }
