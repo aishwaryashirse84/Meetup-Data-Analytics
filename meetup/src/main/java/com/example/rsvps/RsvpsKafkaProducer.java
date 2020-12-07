d156aa26a3a9299e669b1e2552101c9d8da5d43a
Aishwarya shirse <adityashirse84@gmail.com>
2020-12-07 12:45:04 +0530

    done with kafka..getting all the rsvps on console

--- meetup/src/main/java/com/example/rsvps/RsvpsKafkaProducer.java
+++ meetup/src/main/java/com/example/rsvps/RsvpsKafkaProducer.java
@@ -7,17 +7,19 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketMessage;
 
 @Component
+//here,we need a source bcoz we are actually writting a producer for kafka.
 @EnableBinding(Source.class)
 public class RsvpsKafkaProducer {
 
     private static final int SENDING_MESSAGE_TIMEOUT_MS = 10000;
 
     private final Source source;
-
+//constructor
+//in order to obtain a souce object,we just take an advantage of spring dependency injection via class constructor.
     public RsvpsKafkaProducer(Source source) {
         this.source = source;
     }
-
+//following method will send the collected data to kafka via the source object.
     public void sendRsvpMessage(WebSocketMessage<?> message) {
 
         source.output()
